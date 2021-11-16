5074a7fb128f124d035baaf38d15e9e2b285a3d4
Frederik Riedel <frederik@riedel.wtf>
2021-11-16 18:49:36 +0100

    Initial Commit

---
+++ ICE Buddy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ICE Buddy
+//
+//  Created by Frederik Riedel on 16.11.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
